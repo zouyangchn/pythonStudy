7236e827ef0d5e04efaa4e467f6b2637f8e595c5
zouyangchn <595007358@qq.com>
2019-11-25 01:53:48 +0800

    #20191125 操作文件

---
+++ IO编程/操作文件和目录.py
@@ -0,0 +1,21 @@
+import os
+
+print(os.name)
+print(os.environ)
+print(os.environ.get('COMPUTERNAME' ))
+print(os.environ.get('PATH' ))
+
+print(os.path.abspath('.'))
+
+r=os.path.join('E:\code\python\PycharmProjects\pythonStudy\IO编程','test')
+print(r)
+os.mkdir(r)
+os.rmdir(r)
+
+s=os.path.join('E:\code\python\PycharmProjects\pythonStudy\IO编程','hello.py')
+
+t=os.path.split(s)
+u=os.path.splitext(s)
+
+print(t)
+print(u)
