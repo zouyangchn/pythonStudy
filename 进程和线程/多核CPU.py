7bdc8612726d0893888f44605a83d104ce23f896
zouyangchn <595007358@qq.com>
2019-11-28 01:40:20 +0800

    #20191128 正则表达式

---
+++ 进程和线程/多核CPU.py
@@ -0,0 +1,11 @@
+import threading, multiprocessing
+
+def loop():
+    x = 0
+    while True:
+        x = x ^ 1
+
+for i in range(multiprocessing.cpu_count()):
+    print(1)
+    t = threading.Thread(target=loop)
+    t.start()
